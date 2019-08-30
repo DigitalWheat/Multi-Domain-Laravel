bb549e123b46ec978eae39950078c97abb8de30b
Stojan Kukrika <stojan.kukrika@gmail.com>
2019-08-30 09:24:41 +0200

    Update User model
    Fixed User model fillable fields list

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password','is_appadmin'
     ];
 
     /**
